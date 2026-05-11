1bdbb6c341c5587ec5f075337049e89a45b96a17
Mathi27 <mathiyuvarajan.dev@gmail.com>
2026-05-11 12:00:00 +0530

    without-oop

---
+++ 02_Intro_oop/01_without_oop.py
@@ -0,0 +1,10 @@
+# without oop
+
+student1_name = "Mathi"
+student1_age = 21
+
+student2_name = "Yuvarajan"
+student2_age = 22
+
+print(student1_name, student1_age)
+print(student2_name, student2_age)
