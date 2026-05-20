d077f7c3502ce6c9019162ed1b453bff4f7f1877
Mathi27 <mathiyuvarajan.dev@gmail.com>
2026-05-20 12:00:00 +0530

    obj-state

---
+++ 02_Intro_oop/09_two_objects_state.py
@@ -0,0 +1,17 @@
+class BankAccount:
+
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+
+a1 = BankAccount("Mathi", 1000)
+a2 = BankAccount("Kumar", 500)
+
+a1.deposit(200)
+
+print(a1.balance)
+print(a2.balance)
