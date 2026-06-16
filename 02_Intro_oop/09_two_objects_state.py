096580401e47a1ea5b08bb333a4d5ce7990246c6
Mathi27 <mathiyuvarajan.dev@gmail.com>
2026-06-16 19:31:24 +0530

    constructor-basics

--- 02_Intro_oop/09_two_objects_state.py
+++ 02_Intro_oop/09_two_objects_state.py
@@ -1,3 +1,4 @@
+# Mini Project
 class BankAccount:
 
     def __init__(self, owner, balance):
---
+++ 02_Intro_oop/11_constructor_basics.py
@@ -0,0 +1,11 @@
+# Constructor Basics
+
+class Student:
+
+    def __init__(self, name):
+        self.name = name
+
+
+s1 = Student("Mathi")
+
+print(s1.name)
